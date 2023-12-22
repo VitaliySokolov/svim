547d0b5bcce6f99f2b33f16b88cbd9cab8cc5f30
Vitaliy Sokolov <sokolovvitalik@gmail.com>
2023-12-22 19:18:48 +0200

    add git plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,1 +1,36 @@
-return {}
+return {
+  "nvim-lua/plenary.nvim",
+  "MunifTanjim/nui.nvim",
+  {
+    "nvim-tree/nvim-web-devicons",
+    config = { default = true },
+  },
+  { "nacro90/numb.nvim", event = "BufReadPre", config = true },
+  {
+    "stevearc/dressing.nvim",
+    event = "VeryLazy",
+    config = true,
+  },
+  {
+    "rcarriga/nvim-notify",
+    event = "VeryLazy",
+    enabled = true,
+    config = { default = true }, -- same as config = true
+  },
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+    config = true,
+  },
+  {
+    "NeogitOrg/neogit",
+    cmd = "Neogit",
+    config = {
+      integrations = { diffview = true },
+    },
+    keys = {
+      -- <cmd>neogit kind=floating<cr>
+      { "<leader>gs", "<cmd>Neogit<cr>", desc = "Status" },
+    },
+  },
+}
