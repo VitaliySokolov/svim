dd813b39c031203dad70fe98ce78f186792ec8c1
Vitaliy Sokolov <sokolovvitalik@gmail.com>
2025-08-21 19:48:31 +0300

    change tmux theme, update omp, add neotree, gitsigns

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,29 +1,44 @@
+-- return {
+--   "nvim-tree/nvim-tree.lua",
+--   cmd = { "NvimTreeToggle" },
+--   keys = {
+--     { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Explorer" },
+--   },
+--   opts = {
+--     disable_netrw = false,
+--     hijack_netrw = true,
+--     respect_buf_cwd = true,
+--     view = {
+--       number = true,
+--       relativenumber = true,
+--       width = 50,
+--     },
+--     filters = {
+--       custom = { ".git" },
+--     },
+--     sync_root_with_cwd = true,
+--     update_focused_file = {
+--       enable = true,
+--       update_root = true,
+--     },
+--     -- actions = {
+--     --   open_file = {
+--     --     quit_on_open = true,
+--     --   },
+--     -- },
+--   },
+-- }
 return {
-  "nvim-tree/nvim-tree.lua",
-  cmd = { "NvimTreeToggle" },
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  cmd = { "Neotree" },
   keys = {
-    { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Explorer" },
+    { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Explorer" },
   },
-  opts = {
-    disable_netrw = false,
-    hijack_netrw = true,
-    respect_buf_cwd = true,
-    view = {
-      number = true,
-      relativenumber = true,
-    },
-    filters = {
-      custom = { ".git" },
-    },
-    sync_root_with_cwd = true,
-    update_focused_file = {
-      enable = true,
-      update_root = true,
-    },
-    -- actions = {
-    --   open_file = {
-    --     quit_on_open = true,
-    --   },
-    -- },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons", -- optional, but recommended
   },
+  lazy = false, -- neo-tree will lazily load itself
 }
