d2d72b2a67cefa012b96ffbb06d9aeb9d690c4b0
Vitaliy Sokolov <sokolovvitalik@gmail.com>
2026-01-01 11:35:49 +0200

    change suggestion keys, add autopairs

--- min-nvim/lua/plugins/blink-cmp.lua
+++ min-nvim/lua/plugins/blink-cmp.lua
@@ -1,11 +1,11 @@
-local has_words_before = function()
-  local col = vim.api.nvim_win_get_cursor(0)[2]
-  if col == 0 then
-    return false
-  end
-  local line = vim.api.nvim_get_current_line()
-  return line:sub(col, col):match("%s") == nil
-end
+-- local has_words_before = function()
+--   local col = vim.api.nvim_win_get_cursor(0)[2]
+--   if col == 0 then
+--     return false
+--   end
+--   local line = vim.api.nvim_get_current_line()
+--   return line:sub(col, col):match("%s") == nil
+-- end
 
 return {
   'saghen/blink.cmp',
@@ -27,15 +27,17 @@ return {
     keymap = {
       preset = 'none',
       -- If completion hasn't been triggered yet, insert the first suggestion; if it has, cycle to the next suggestion.
-      ['<Tab>'] = {
-        function(cmp)
-          if has_words_before() then
-            return cmp.insert_next()
-          end
-        end,
-        'fallback',
-      },
-      ['<S-Tab>'] = { 'insert_prev' },
+      -- ['<Tab>'] = {
+      --   function(cmp)
+      --     if has_words_before() then
+      --       return cmp.insert_next()
+      --     end
+      --   end,
+      --   'fallback',
+      -- },
+      -- ['<S-Tab>'] = { 'insert_prev' },
+      ['<Tab>'] = { 'accept', 'fallback' },
+      ['<S-Tab>'] = { 'hide', 'fallback' },
       ['<C-e>'] = { 'hide', 'fallback' },
       ['<CR>'] = { 'accept', 'fallback' },
       ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
--- min-nvim/lua/plugins/init.lua
+++ min-nvim/lua/plugins/init.lua
@@ -34,5 +34,12 @@ return {
             }
         }
       end,
+    },
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+        -- use opts = {} for passing setup options
+        -- this is equivalent to setup({}) function
     }
 }
